9e2fc3e685815ebad787f9cd15ac13ec3a204ffa
skrenek <lkrenek@gmail.com>
2020-02-24 16:22:12 -0500

    Initial

---
+++ src/config.js
@@ -0,0 +1,23 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "ZDU0Y2MwMTM4MjE1NjgwZDQzNWQyNjM4MjUwNmE4Y2Q5ZjcxZjIzNQ==",
+
+  // Your Github UserName
+  githubUserName: "scrimbash",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "blog",
+
+  // Your Personal Blog Title
+  title : "Test Blog" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "A Test Developer Blog",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    titleColor: '#ff5252',
+    subtitleColor: '#37474f',
+  },
+};
